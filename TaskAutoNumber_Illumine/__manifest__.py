6556de226632e8ec56a12b91b9c282d22c22fd4c
kamalitha <kamalitha@minacto.com>
2021-08-21 16:09:58 +0530

    2021-08-21
    Signed-off-by: kamalitha <kamalitha@minacto.com>

--- TaskAutoNumber_Illumine/__manifest__.py
+++ TaskAutoNumber_Illumine/__manifest__.py
@@ -7,9 +7,9 @@
     "category": 'Sales',
     "website": "https://www.minacto.com",
     "description": """Generates Project Task Number automatically based on the selected Project""",
-    'author': 'Kamalitha Gunasinghe',
+    'author': 'Kamalitha',
     'company': 'Illumine',
-    'maintainer': 'Kamalitha Gunasinghe',
+    'maintainer': 'Kamalitha',
     "depends": [
         'project',
     ],
