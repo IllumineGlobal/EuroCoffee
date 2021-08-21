142a77be19541237b7d6a3afad5aeb5d9b7673af
kamalitha <68263759+kamalitha@users.noreply.github.com>
2021-08-21 15:56:04 +0530

    Changed the Description in Manifest

--- TaskAutoNumber_Illumine/__manifest__.py
+++ TaskAutoNumber_Illumine/__manifest__.py
@@ -2,11 +2,11 @@
 
 {
     "name": "Project Task Auto Number Module",
-    "summary": "Generates Project Task Number automatically based on the selected product",
+    "summary": "Generates Project Task Number automatically based on the selected Project",
     "version": "1.0.0",
     "category": 'Sales',
     "website": "https://www.minacto.com",
-    "description": """Generates Project Task Number automatically based on the selected product""",
+    "description": """Generates Project Task Number automatically based on the selected Project""",
     'author': 'Kamalitha',
     'company': 'Illumine',
     'maintainer': 'Kamalitha',
