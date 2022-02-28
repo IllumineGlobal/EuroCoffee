67790c878181170f07d315ccaac53637fb739af3
kamalitha <68263759+kamalitha@users.noreply.github.com>
2022-02-28 11:00:45 +0530

    Custom POS Receipt V 1.0.1
    This module will add the VAT number and VAT INVOICE text to the POS receipt

---
+++ custom_pos_receipt/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'POS Custom Receipt',
+    'version': '1.0',
+    'category': 'Sales/Point of Sale',
+    'summary': 'This module will add the VAT number and VAT INVOICE text to the POS receipt',
+    'website': 'http://www.minacto.com',
+    'author': 'Kamalitha Gunasinghe',
+
+    'description': "Customized POS Receipt by Illumine",
+    'depends': ['base', 'point_of_sale'],
+    "data": [],
+    'demo': [],
+    'qweb': ['static/src/xml/pos.xml'],
+    'installable': True,
+}
