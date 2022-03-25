1769f469f695f2e695598e9c513f2f1554382f1d
kamalitha <68263759+kamalitha@users.noreply.github.com>
2022-03-25 12:02:21 +0530

    Add files via upload

--- AssetManager/models/project_task.py
+++ AssetManager/models/project_task.py
@@ -3,9 +3,9 @@ from odoo import models, fields, api, _
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    description = fields.Many2one('asset.master', string="Asset")
+    asset_id = fields.Many2one('asset.master', string="Asset")
 
     @api.onchange('partner_id')
     def get_asset_for_project(self):
         for rec in self:
-            description = fields.One2many('asset.master', rec.partner_id.id, string="Asset")
+            asset_id = fields.One2many('asset.master', rec.partner_id.id, string="Asset")
